537e9667b167a2bc38009c6c44e84307b54c34f2
Peter Unger <peps.unger@gmail.com>
2019-02-14 21:43:38 +0100

    Implement Node component

--- src/lib/Node.tsx
+++ src/lib/Node.tsx
@@ -1,30 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Draggable from 'react-draggable';
+import { NodeSpecification, NodeInput, NodeOutput } from './interfaces';
+import Connector from './Connector';
+
+export interface NodeProps {
+  input?: any[],
+  output?: any[],
+  spec: NodeSpecification,
+  uid?: string;
+}
 
 const NodeElement = styled.div`
   position: fixed;
   top: 50%;
   left: 50%;
-  display: flex;
-  min-width: 100px;
-  min-height: 100px;
+  display: block;
   background-color: #fff;
-  border-radius: 5px;
-  box-shadow: 0 1px 7px rgba(0,0,0,.05);
-  border: 1px solid rgba(0,0,0,.04);
+  border-radius: 8px;
+  box-shadow: 0 1px 7px rgba(0,0,0,.15);
+  border: 1px solid rgba(0,0,0,.1);
 `;
 
-const eventLogger = (e: MouseEvent, data: Object) => {
-  console.log('Event: ', e);
-  console.log('Data: ', data);
-};
+const Head = styled.div`
+  padding: 5px 1em;
+  background-color: #808080;
+  color: #fff;
+  text-align: center;
+  font-weight: bold;
+  border-radius: 8px 8px 0 0;
+`;
 
-const Node = () => {
+const Connectors = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr;
+  grid-gap: 1em;
+  margin: 0;
+`;
+
+const renderConnectors = (connectors: Array<NodeInput | NodeOutput>, mode: string = 'in'): Array<NodeInput | NodeOutput> => {
+  return Object.keys(connectors).map((key: string, index) => {
+    const connector = connectors[key];
+    return (<Connector key={index} mode={mode} {...connector}>{connector.name}</Connector>)
+  });
+}
+
+const Node: React.SFC<NodeProps> = (props) => {
+
   return (
     <Draggable>
       <NodeElement>
-        node
+        <Head>{props.spec.type}</Head>
+        <Connectors>
+          <div>
+            {renderConnectors(props.spec.inputs, 'in')}
+          </div>
+          <div>
+            {renderConnectors(props.spec.outputs, 'out')}
+          </div>
+        </Connectors>
       </NodeElement>
     </Draggable>
   );
